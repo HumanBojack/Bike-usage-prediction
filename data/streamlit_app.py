a041d69888d22704f60134301e3ea1cb5616c593
HumanBojack <spychalar59@gmail.com>
2022-03-31 14:40:25 +0200

    connection to api for unique prediction

--- data/streamlit_app.py
+++ data/streamlit_app.py
@@ -4,13 +4,14 @@ import streamlit as st
 from sklearn.base import BaseEstimator,TransformerMixin
 import pickle 
 import requests
-import datetime
+from datetime import datetime
 import json
 import plotly.express as px
 import matplotlib.pyplot as plt
 import numpy as np
 
-# API METEO
+# API METEO => Make changes to not have api key in the code and to transform the response in a 1-4 integer
+# 
 
 api_key = "6f6890ed8c566a3b0f5763b583f17182"
 
@@ -55,57 +56,42 @@ model = pickle.load(open("lightgbm.pkl", "rb"))
 # Sidebar
 
 # CUSTOM DATE PREDICTION
-d = st.sidebar.date_input(
-     "Jour",
-     datetime.date(2022, 3, 28))
+current_date = st.sidebar.checkbox("Date actuelle", value=True)
 
-time = st.sidebar.time_input('Prédire cette heure', datetime.time(8, 45))
+if not current_date:
 
-season = st.sidebar.number_input('Insert season')
+    d = st.sidebar.date_input(
+        "Jour",
+        datetime.now())
+    time = st.sidebar.time_input('Prédire cette heure', datetime.now())
+    date_side = datetime.combine(d, time)
 
-holiday = st.sidebar.number_input('Insert holiday')
+weather = st.sidebar.number_input('Insert weather', step=1)
 
-workingday = st.sidebar.number_input('Insert workingday')
+temp = st.sidebar.number_input('Insert temp', step=1)
 
-weather = st.sidebar.number_input('Insert weather')
+atemp = st.sidebar.number_input('Insert atemp', step=1)
 
-temp = st.sidebar.number_input('Insert temp')
+humidity = st.sidebar.number_input('Insert humidity', step=1)
 
-atemp = st.sidebar.number_input('Insert atemp')
-
-humidity = st.sidebar.number_input('Insert humidity')
-
-windspeed = st.sidebar.number_input('Insert windspeed')
+windspeed = st.sidebar.number_input('Insert windspeed', step=1)
 
 validation = st.sidebar.button('Validez votre choix')
 
-date_side = datetime.datetime.combine(d, time)
-
-df = pd.DataFrame(columns=['datetime', 'season', 'holiday', 'workingday', 'weather', 'temp',
-       'atemp', 'humidity', 'windspeed'])
-
 if validation : 
-    
-    # Creation du dataset
-    df_side = []
-    lst_side = [] 
-    
-    lst_side = [date_side, season,holiday,workingday,weather,temp,atemp,humidity, windspeed]
-    
-    df_side = df_side.append(pd.Series(lst_side, index =['datetime', 'season', 'holiday', 'workingday', 'weather', 'temp',
-    'atemp', 'humidity', 'windspeed'] ), ignore_index=True)
-    # Prediction data
-    
-    transform_data_side = model["preprocessor"].transform(df_side)
-    
-    pred_side = model['model'].predict(transform_data_side)
-    
-    st.dataframe(pred_side)
-    
+
+    url = f"http://romainbs.azurewebsites.net/predict/?weather={weather}&temp={temp}&felt_temp={atemp}&humidity={humidity}&windspeed={windspeed}"
+    if not current_date:
+        url += f"&date_time={date_side}"
+
+    response = requests.get(url)
+    data = json.loads(response.text)
+
+    st.header(url)
+    st.header(data)
 
 
 # LIVE TIME PREDICTION
-    
 for i in range(0,47):
     
     j = meteo_data["hourly"][i]
